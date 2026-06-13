67cd6c39efa83658de5a51dc8465da9eff714891
isaevabeatrissa <isaeva.beatrissa@gmail.com>
2026-06-13 23:48:57 +0300

    changed block speed

---
+++ Constants.h
@@ -0,0 +1,32 @@
+#pragma once
+
+constexpr float WINDOW_WIDTH = 800.f;
+constexpr float WINDOW_HEIGHT = 600.f;
+
+constexpr float BALL_RADIUS = 10.f;
+
+constexpr float BALL_SPEED = 3.f;
+
+constexpr float BONUS_SPEED = 3.f;
+
+constexpr float CARRIAGE_SPEED = 7.f;
+
+constexpr float UNBREAKABLE_HP = 999;
+
+constexpr float BALL_START_X = 400.f;
+constexpr float BALL_START_Y = 400.f;
+
+constexpr float SLOW_BALL_SPEED = 2.f;
+
+constexpr float DEFEAT = 5;
+
+constexpr float BIG_CARRIAGE_WIDTH = 180.f;
+constexpr float CARRIAGE_WIDTH = 120.f;
+constexpr float CARRIAGE_HEIGHT = 20.f;
+
+constexpr float SHIELD_BOUNCE_Y = 560.f;
+
+constexpr float BLOCK_WIDTH = 60.f;
+constexpr float BLOCK_HEIGHT = 25.f;
+
+
